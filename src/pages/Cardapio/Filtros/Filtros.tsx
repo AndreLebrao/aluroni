2f913bdca039e033acb47571424bd217c1f43733
AndreLebrao <andledrao@gmail.com>
2022-08-03 12:32:11 -0300

    feature (Filtro) : using classnames package

--- src/pages/Cardapio/Filtros/Filtros.tsx
+++ src/pages/Cardapio/Filtros/Filtros.tsx
@@ -1,5 +1,6 @@
 import styles from "./Filtros.module.scss";
 import filtros from "./filtros.json";
+import classNames from "classnames";
 
 type IOpcao = typeof filtros[0];
 interface props {
@@ -18,9 +19,19 @@ export default function Filtros(props: props) {
     <div className={styles.filtros}>
       {filtros.map((opcao) => (
         <button
-          className={`${styles.filtros__filtro} ${
-            props.filtro == opcao.id ? styles["filtros__filtro--ativo"] : ""
-          }`}
+          className={classNames({
+            // with classnames package
+            // when creating object keys, if it is surrounded by brackets,
+            //  the key value will be the value of what's inside brackets
+            // otherwise, it will just be the value itself
+            // eg: here without brackets, the key would be "styles.filtros__filtro"
+            [styles.filtros__filtro] : true,
+            [styles["filtros__filtro--ativo"]] : props.filtro == opcao.id
+          })}
+        //   default way to do this
+        //   className={`${styles.filtros__filtro} ${
+        //     props.filtro == opcao.id ? styles["filtros__filtro--ativo"] : ""
+        //   }`}
           key={opcao.id}
           onClick={() => {
             selecionarFiltro(opcao);
