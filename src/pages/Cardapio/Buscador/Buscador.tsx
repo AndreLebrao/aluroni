8a4ef1f2f4d6671436cb7dc9b57916f23f9e222c
AndreLebrao <andledrao@gmail.com>
2022-08-03 10:53:20 -0300

    feature (buscador) : search bar

---
+++ src/pages/Cardapio/Buscador/Buscador.tsx
@@ -0,0 +1,19 @@
+import sytles from "./Buscador.module.scss";
+import { CgSearch } from "react-icons/cg";
+
+interface props {
+  busca: string;
+  setBusca: React.Dispatch<React.SetStateAction<string>>;
+}
+
+export default function Buscador(props: props) {
+  return (
+    <div className={sytles.buscador}>
+      <input
+        value={props.busca}
+        onChange={(evento) => props.setBusca(evento.target.value)}
+      />
+      <CgSearch size={20} color="#4C4D4E" />
+    </div>
+  );
+}
