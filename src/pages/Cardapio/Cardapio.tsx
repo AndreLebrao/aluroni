371183add8bc8d03c1b08ad959f41f2e00c721b7
AndreLebrao <andledrao@gmail.com>
2022-08-04 11:11:45 -0300

    feature (Itens) : creating items list

--- src/pages/Cardapio/Cardapio.tsx
+++ src/pages/Cardapio/Cardapio.tsx
@@ -4,6 +4,8 @@ import { useState } from "react";
 import Buscador from "./Buscador/Buscador";
 import Filtros from "./Filtros/Filtros";
 import Ordenador from "./Ordenador/Ordenador";
+import Itens from './Itens/Itens';
+
 export default function Cardapio() {
   const [busca, setBusca] = useState<string>("");
   const [filtro, setFiltro] = useState<number | null>(null);
@@ -23,6 +25,7 @@ export default function Cardapio() {
           <Filtros filtro={filtro} setFiltro={setFiltro} />
           <Ordenador ordenador={ordenador} setOrdenador={setOrdenador} />
         </div>
+        <Itens />
       </section>
     </main>
   );
